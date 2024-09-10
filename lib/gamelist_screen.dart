230e1afd4f99f9bc7498811ae87475b1958ea888
N3RO <jho20455744@gmail.com>
2024-09-10 14:37:54 +0900

    연속 실행 방지

--- lib/gamelist_screen.dart
+++ lib/gamelist_screen.dart
@@ -10,6 +10,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'addgame_screen.dart';
 import 'setting_screen.dart';
 
+/*
+메인화면 클래스
+*/
+
 class GameListScreen extends StatefulWidget {
   const GameListScreen({super.key});
 
@@ -18,71 +22,106 @@ class GameListScreen extends StatefulWidget {
 }
 
 class _GameListScreenState extends State<GameListScreen> {
+  // 게임 목록을 저장하는 리스트
   List<Map<String, String>> games = [];
+
+  // 각 게임 항목의 호버 상태를 저장하는 리스트
   List<bool> hoveredStates = [];
+  bool isLaunching = false;
+
+  // 부제목
   String subtitle = "GAME LAUNCHER";
 
+  // 설정 화면 접근을 위한 비밀번호
   final String _settingsPassword = 'xladhem1234';
 
   @override
   void initState() {
     super.initState();
+    // 위젯이 초기화될 때 게임 목록을 로드
     _loadGames();
   }
 
+  // 게임 목록과 부제목을 로드하는 함수
   Future<void> _loadGames() async {
+    // SharedPreferences 인스턴스를 가져옴
     final prefs = await SharedPreferences.getInstance();
+    // 'games' 키로 저장된 JSON 문자열을 가져옴
     final String? gamesJson = prefs.getString('games');
     setState(() {
       if (gamesJson != null) {
+        // JSON 문자열을 Map으로 디코딩
         final Map<String, dynamic> data = json.decode(gamesJson);
+        // 'games' 키의 값을 List<Map<String, String>>으로 변환
         games = (data['games'] as List<dynamic>).map((game) {
           return Map<String, String>.from(game);
         }).toList();
+        // 'subtitle' 키의 값을 가져오거나 기본값 설정
         subtitle = data['subtitle'] ?? "GAME LAUNCHER";
       } else {
-        subtitle = "GAME LAUNCHER"; // 기본값 설정
+        // 저장된 데이터가 없으면 기본 부제목 설정
+        subtitle = "GAME LAUNCHER";
       }
+      // 호버 상태 업데이트
       _updateHoveredStates();
     });
   }
 
+  // 게임 목록과 부제목을 저장하는 함수
   Future<void> _saveGames() async {
+    // SharedPreferences 인스턴스를 가져옴
     final prefs = await SharedPreferences.getInstance();
+    // 게임 목록과 부제목을 포함하는 데이터 맵 생성
     final data = {
       'games': games.map((game) => Map<String, dynamic>.from(game)).toList(),
       'subtitle': subtitle,
     };
+    // 데이터를 JSON 문자열로 인코딩하여 SharedPreferences에 저장
     await prefs.setString('games', json.encode(data));
   }
 
+  // 로컬 파일 시스템에서 games.json 파일의 경로를 가져오는 함수
   Future<File> _getLocalFile() async {
+    // 애플리케이션의 문서 디렉토리 경로를 가져옴
     final directory = await getApplicationDocumentsDirectory();
+    // 문서 디렉토리 내의 'games.json' 파일에 대한 File 객체를 반환
     return File('${directory.path}/games.json');
   }
 
   void _updateHoveredStates() {
     setState(() {
-      hoveredStates = List.generate(games.length, (_) => false);
+      hoveredStates = List.generate(games.length, (_) => false); // 호버 상태 업데이트
     });
   }
 
+  // 새 게임을 추가하는 함수
   void _addNewGame() async {
+    // AddGameScreen으로 이동하여 결과를 기다림
     final result = await Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const AddGameScreen()),
     );
 
+    // 결과가 null이 아니고 Map<String, String> 타입인 경우
     if (result != null && result is Map<String, String>) {
       setState(() {
+        // 게임 목록에 새 게임 추가
         games.add(result);
+        // 호버 상태 업데이트
         _updateHoveredStates();
       });
+      // 변경된 게임 목록 저장
       _saveGames();
     }
   }
 
   Future<void> _launchGame(String execPath) async {
+    if (isLaunching) return;
+
+    setState(() {
+      isLaunching = true;
+    });
+
     try {
       if (Platform.isWindows) {
         await Process.run(execPath, [], runInShell: true);
@@ -91,6 +130,10 @@ class _GameListScreenState extends State<GameListScreen> {
       }
     } catch (e) {
       print('Error launching game: $e');
+    } finally {
+      setState(() {
+        isLaunching = false;
+      });
     }
   }
 
